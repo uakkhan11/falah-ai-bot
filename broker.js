1c3d7d6fd09f13483db9666e0f4c8d423d1af0de
uakkhan11 <uakkhan11@gmail.com>
2025-06-10 04:30:35 +0530

    Update broker.js

--- broker.js
+++ broker.js
@@ -1,7 +1,7 @@
 // broker.js
 
 // The URL of your proxy endpoint (replace with your deployment URL)
-const BROKER_PROXY = 'https://your-proxy.example.com/executeOrder';
+const BROKER_PROXY = 'https://script.google.com/macros/s/AKfycbyc16vDXTn_7gfhnmFHfxpYQEpxROgVIb7mui1Wx6fLlHC3hpvJEnbJwJ-cTGw1socv/exec';
 
 async function placeOrder(broker, { stock, entry }) {
   // Send through your proxy so you don't expose keys in the browser

d5563533d30c91a17e4330adea04800166abaca3
uakkhan11 <uakkhan11@gmail.com>
2025-06-22 00:20:11 +0530

    Delete .devcontainer directory

--- .devcontainer/postCreateCommand.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "📦 Installing Python dependencies..."
-pip install --upgrade pip
-pip install streamlit pandas kiteconnect gspread oauth2client requests
-echo "✅ Dependencies installed successfully."

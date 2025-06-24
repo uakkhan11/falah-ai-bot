f5e2830e0821b71b2b966528c3d6c20f8e663721
uakkhan11 <uakkhan11@gmail.com>
2025-06-25 00:29:49 +0530

    Update test_secrets.py

--- test_secrets.py
+++ test_secrets.py
@@ -1,10 +1,10 @@
 # test_secrets.py
 import toml
 
-with open("/root/falah-ai-bot/.streamlit/secrets.toml", "r") as f:
+with open(".streamlit/secrets.toml", "r") as f:
     secrets = toml.load(f)
 
 print("✅ API Key:", secrets["zerodha"]["api_key"])
 print("✅ Access Token:", secrets["zerodha"]["access_token"])
-print("✅ Google Sheet Key:", secrets["google_sheet_key"])
-print("✅ Telegram Chat ID:", secrets["telegram_chat_id"])
+print("✅ Google Sheet Key:", secrets["global"]["google_sheet_key"])
+

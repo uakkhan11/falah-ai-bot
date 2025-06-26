660c169d2e2c91ac3752e0ffb40621382ed653fe
uakkhan11 <uakkhan11@gmail.com>
2025-06-26 10:56:03 +0530

    Update manual_token_test.py

--- manual_token_test.py
+++ manual_token_test.py
@@ -1,7 +1,7 @@
 from kiteconnect import KiteConnect
 
 kite = KiteConnect(api_key="ijzeuwuylr3g0kug")
-kite.set_access_token("8e1HmKugtXQB12ncAYEDsfIcdbett6hK")  # paste the exact token printed by generate_session()
+kite.set_access_token("5xRl14oo95VnhV5iClVi1A26BXfC2wBW")  # paste the exact token printed by generate_session()
 
 profile = kite.profile()
 print("✅ Profile loaded:", profile["user_name"])

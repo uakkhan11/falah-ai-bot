255d4606dd89fd61dac7fa84c33a079464f122df
uakkhan11 <uakkhan11@gmail.com>
2025-06-26 13:51:51 +0530

    Delete manual_token_test.py

--- manual_token_test.py
+++
@@ -1,7 +0,0 @@
-from kiteconnect import KiteConnect
-
-kite = KiteConnect(api_key="ijzeuwuylr3g0kug")
-kite.set_access_token("5xRl14oo95VnhV5iClVi1A26BXfC2wBW")  # paste the exact token printed by generate_session()
-
-profile = kite.profile()
-print("✅ Profile loaded:", profile["user_name"])

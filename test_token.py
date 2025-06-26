a51ee486e0a90d382b61f5204b5a445af567ab26
uakkhan11 <uakkhan11@gmail.com>
2025-06-26 13:54:18 +0530

    Delete test_token.py

--- test_token.py
+++
@@ -1,7 +0,0 @@
-from kiteconnect import KiteConnect
-
-kite = KiteConnect(api_key="ijzeuwuylr3g0kug")
-kite.set_access_token("8e1HmKugtXQB12ncAYEDsfIcdbett6hK")
-
-profile = kite.profile()
-print("✅ Logged in as:", profile["user_name"])

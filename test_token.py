37c4252994721100f8d55fde93336114d3c316fe
uakkhan11 <uakkhan11@gmail.com>
2025-06-26 10:43:17 +0530

    Update test_token.py

--- test_token.py
+++ test_token.py
@@ -1,7 +1,7 @@
 from kiteconnect import KiteConnect
 
 kite = KiteConnect(api_key="ijzeuwuylr3g0kug")
-kite.set_access_token("eukSPf050njsE6ucsN9RbIg5RbSpaosh")
+kite.set_access_token("053g791myYRvFIjV49pFsFz51W72k0gT")
 
 profile = kite.profile()
 print("✅ Logged in as:", profile["user_name"])

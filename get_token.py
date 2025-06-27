fb8581fa33455ff70d8a235c1452db3734f4a3e7
uakkhan11 <uakkhan11@gmail.com>
2025-06-27 11:58:49 +0530

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -2,7 +2,7 @@ from kiteconnect import KiteConnect
 
 api_key = "ijzeuwuylr3g0kug"
 api_secret = "yy1wd2wn8r0wx4mus00vxllgss03nuqx"
-request_token = "ha5pPfUdUYzzisANQOQjAUwzvuxkI5gC"   # from URL
+request_token = "ZKT2LaZz6rAD1KqK0F37fysMnk0pVlZr"   # from URL
 
 kite = KiteConnect(api_key=api_key)
 data = kite.generate_session(request_token, api_secret=api_secret)

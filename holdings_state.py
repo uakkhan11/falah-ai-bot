8b0488f369462c6f34c6ab5e6e885c11903f4f6b
uakkhan11 <uakkhan11@gmail.com>
2025-06-29 02:44:51 +0530

    Update holdings_state.py

--- holdings_state.py
+++ holdings_state.py
@@ -7,9 +7,17 @@ def load_previous_exits(exit_log_file="/root/falah-ai-bot/exited_stocks.json"):
     with open(exit_log_file, "r") as f:
         return json.load(f)
 
-def update_exit_log(exit_log_file, symbol):
-    exited = load_previous_exits(exit_log_file)
-    if symbol not in exited:
-        exited.append(symbol)
-        with open(exit_log_file, "w") as f:
-            json.dump(exited, f)
+import json
+from datetime import datetime
+
+def update_exit_log(filepath, symbol):
+    try:
+        with open(filepath, "r") as f:
+            data = json.load(f)
+    except:
+        data = {}
+
+    data[symbol] = datetime.now().strftime("%Y-%m-%d")
+
+    with open(filepath, "w") as f:
+        json.dump(data, f, indent=2)

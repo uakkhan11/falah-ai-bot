86a954113ccc9207775b23a5867e0ab456720abf
uakkhan11 <uakkhan11@gmail.com>
2025-07-04 22:15:14 +0530

    Update smart_scanner.py

--- smart_scanner.py
+++ smart_scanner.py
@@ -107,7 +107,7 @@ def run_smart_scan():
             
         print(f"👉 {sym} | Score: {score} | Reasons: {reasons}")
         
-        if score >= 2:
+        #if score >= 2:
             results.append({
                 "Symbol": sym,
                 "CMP": ltp,

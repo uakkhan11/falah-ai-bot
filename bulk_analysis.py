08e934cdcd8ce64e2b6c09525ac87f0640683ccc
uakkhan11 <uakkhan11@gmail.com>
2025-07-09 01:24:57 +0530

    Update bulk_analysis.py

--- bulk_analysis.py
+++ bulk_analysis.py
@@ -1,11 +1,6 @@
-# bulk_analysis.py
-
 from stock_analysis import analyze_stock
 
 def analyze_multiple_stocks(kite, symbols):
-    """
-    Analyze multiple symbols and return a list of dicts.
-    """
     results = []
     for sym in symbols:
         try:

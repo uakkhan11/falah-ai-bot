84696d2b8992a33371188eb2eaa382157d2d1d5f
uakkhan11 <uakkhan11@gmail.com>
2025-07-10 01:51:29 +0530

    Update run_backtest.py

--- run_backtest.py
+++ run_backtest.py
@@ -1,12 +1,16 @@
 from backtester import Backtester
+from strategies import sma_strategy
 
 bt = Backtester(
     data_dir="/root/falah-ai-bot/historical_data",
-    symbols=["INFY", "TCS", "HDFCBANK"],
+    symbols=["INFY","TCS","HDFCBANK"],
     initial_capital=100000,
     slippage_pct=0.002,
     commission_per_trade=20,
-    risk_per_trade_pct=2
+    risk_per_trade_pct=2,
+    walk_forward_train_days=90,
+    walk_forward_test_days=30,
+    strategy_func=sma_strategy
 )
 
 bt.run()

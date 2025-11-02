86e6433b1ea14f32378a8bda4ffb0f973f39d1a8
uakkhan11 <uakkhan11@gmail.com>
2025-11-02 22:31:37 +0530

    Update Export_SuperTrend.py

--- Export_SuperTrend.py
+++ Export_SuperTrend.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-# Define SuperTrend function
+# Define SuperTrend function with loc assignment
 def supertrend(df, period=10, multiplier=3):
     df['tr'] = np.max((df['high'] - df['low'],
                       abs(df['high'] - df['close'].shift(1)),
@@ -12,12 +12,12 @@ def supertrend(df, period=10, multiplier=3):
     df['lowerband'] = hl2 - (multiplier * df['atr'])
     df['supertrend'] = 1  # Initialize as bullish
     for i in range(1, len(df)):
-        if df['close'].iloc[i] > df['upperband'].iloc[i-1]:
-            df['supertrend'].iloc[i] = 1  # Bullish
-        elif df['close'].iloc[i] < df['lowerband'].iloc[i-1]:
-            df['supertrend'].iloc[i] = -1  # Bearish
+        if df.loc[i, 'close'] > df.loc[i-1, 'upperband']:
+            df.loc[i, 'supertrend'] = 1  # Bullish
+        elif df.loc[i, 'close'] < df.loc[i-1, 'lowerband']:
+            df.loc[i, 'supertrend'] = -1  # Bearish
         else:
-            df['supertrend'].iloc[i] = df['supertrend'].iloc[i-1]
+            df.loc[i, 'supertrend'] = df.loc[i-1, 'supertrend']
     return df
 
 # Set your symbol
